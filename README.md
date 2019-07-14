74a512026f43fe9dbb50f2fd7ec06ec191900e3c
Dragos Nedelcu <dragosgn@gmail.com>
2019-07-14 17:41:36 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vanilla-javascript-class
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+console.log("first");
+console.log("The pig is watchin you!!");
+console.log("Piggy is watching me, he defietly needs to eat more salad!");
